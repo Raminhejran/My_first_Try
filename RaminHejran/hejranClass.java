7011f3632948b29a6714f9e7506bc3ff446cef70
Ramin Hejran <Raminhejran62@gmail.com>
2023-08-21 15:53:51 -0400

    initial commit from Hejran

---
+++ RaminHejran/hejranClass.java
@@ -0,0 +1,9 @@
+package RaminHejran;
+
+public class hejranClass {
+
+	public static void main(String[] args) {
+	System.out.println("Ramin Hejran is trying to work on his first project with git and gitHub");
+	}
+
+}
